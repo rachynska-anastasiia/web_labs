1f4185a6548b6d27e8e3ff5338fc8dc1a940aab8
rachynska-anastasiia <rachynska.anastasiia.work@gmail.com>
2025-11-21 20:51:12 +0200

    Правки 21.11

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/courses", (req, res) => res.render("lab1courses"));
 app.get("/computer_inside", (req, res) => res.render("computer_inside"));
 app.get("/computer_vision", (req, res) => res.render("computer_vision"));
 app.get("/cyber_detective", (req, res) => res.render("cyber_detective"));
-app.get("/html_css", (req, res) => res.render("html_css"));
+app.get("/html_css", (req, res) => res.render("web_course"));
 app.get("/pixel_art", (req, res) => res.render("pixel_art"));
 app.get("/unity_what", (req, res) => res.render("unity_what"));
 
